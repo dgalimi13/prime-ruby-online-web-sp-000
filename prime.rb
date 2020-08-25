ee0ce3cde4872d2a8bb51d889f1cc29d6d092f9f
danny galimi <dannygalimi13@aim.com>
2020-08-25 23:43:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-def prime?(max)
+
+def prime?(n)
+    if n <= 1
+      return false
+    else 
+        if (2...n).any? { |i| n % i  == 0}
+            return false
+        else
+            return true
+        end 
+    end
+end
