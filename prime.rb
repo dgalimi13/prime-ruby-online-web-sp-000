cf26f6dc10f4a84b8b73c59e283744c741696a4d
danny galimi <dannygalimi13@aim.com>
2020-08-25 23:39:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(number)
-  i = 2
-  if number%i == 0
-    return true
-else
-  return false
-end
-end
-  
+ i = 2
+  while i < max
+    is_divisible = ((number % i) == 0)
+    if is_divisible
+      # divisor found; stop and return false!
+      return false
+    end
+
+    i += 1
+  end
